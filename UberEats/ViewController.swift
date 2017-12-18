5b2f6ca139923ec9ff63f3e28656049e5cf2ef24
Sean Nam <namsangwoo1@gmail.com>
2017-12-18 15:58:56 -0800

    Initial commit, login screen

---
+++ UberEats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UberEats
+//
+//  Created by Sean Nam on 12/18/17.
+//  Copyright © 2017 seannam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
