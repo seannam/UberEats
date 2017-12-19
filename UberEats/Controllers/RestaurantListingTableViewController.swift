88c491dda2bff92f16040f8a8a2930426484aab2
Sean Nam <namsangwoo1@gmail.com>
2017-12-18 17:18:17 -0800

    Added UI for Restaurant Listings

--- UberEats/Controllers/RestaurantListingTableViewController.swift
+++ UberEats/Controllers/RestaurantListingTableViewController.swift
@@ -24,3 +24,21 @@ class RestaurantListingTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
 }
+
+// MARK: - UITableViewDataSource
+
+extension RestaurantListingTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "restaurantCell", for: indexPath)
+        cell.selectionStyle = .none
+        return cell
+    }
+}
